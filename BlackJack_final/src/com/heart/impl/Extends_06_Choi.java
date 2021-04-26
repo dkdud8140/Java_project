c01ba13f55e7ad225c93697074f01e3d96a7581f
dkdud8140 <cho.ay4@gmail.com>
2021-04-26 12:45:01 +0900

    real_final_2

--- BlackJack_final/src/com/heart/impl/Extends_06_Choi.java
+++ BlackJack_final/src/com/heart/impl/Extends_06_Choi.java
@@ -8,46 +8,51 @@ public class Extends_06_Choi extends Extends_05_Cho {
 	@Override
 	public void scoreResult() {
 
-		// 인슈어런스 진행했을 시, 딜러가 블랙잭이 맞았다면 인슈어런스 금액 돌려줌
-		if (inYN && voD.getBj()) {
-			System.out.println("인슈어런스 보험금을 돌려드립니다.");
-			System.out.println((inMoney * 2) + "원 지급");
-			voP.setMoney(voP.getMoney() + (inMoney * 2));
-		}
+	      // 인슈어런스 진행했는데 딜러가 블랙잭이 맞았다면 인슈어런스 금액 돌려줌
+	      if (inYN && voD.getBj()) {
+	         System.out.println("인슈어런스 보험금을 돌려드립니다.");
+	         System.out.println((inMoney * 2) + "원 지급");
+	         voP.setMoney(voP.getMoney() + (inMoney * 2));
+	      }
+	      // 인슈어런스 안했으면 넘어감
 
-		Integer pScore = voP.getScore(); // 플레이어의 최종 점수
-		Integer dScore = voD.getScore(); // 딜러의 최종 점수
+	      Integer pScore = voP.getScore(); // 플레이어의 최종 점수
+	      Integer dScore = voD.getScore(); // 딜러의 최종 점수
 
-		// 2021.04.23 블랙잭으로 인한 승부계산 <-> 블랙잭 아닐 때 승부 계산 함수 자리 바꾸고 return 추가
-		if (voP.getBj()) { // 플레이어가 블랙잭이라면
-			if (voD.getBj()) { // 딜러가 블랙잭인지도 확인하고 딜러도 블랙잭이라면
-				push(); // 무승부
-				return; // 아래 if 문은 실행되지 않게 return 해버리기
-			} else { // 딜러는 블랙잭이 아니라면
-				win_bj(); // 플레이어 승리 ~!
-				return;
-			}
-		}
+	      // 2021.04.23 블랙잭으로 인한 승부계산 <-> 블랙잭 아닐 때 승부 계산 함수 자리 바꾸고 return 추가
+	      if (voP.getBj()) { // 플레이어가 블랙잭이라면
+	         if (voD.getBj()) { // 딜러가 블랙잭인지도 확인하고 딜러도 블랙잭이라면
+	            push(); // 무승부
+	            return; // 아래 if 문은 실행되지 않게 return 해버리기
+	         } else { // 딜러는 블랙잭이 아니라면
+	            win_bj(); // 플레이어 승리 ~!
+	            return;
+	         }
+	      } else if (voD.getBj()) { // 플레이어가 블랙잭이 아니고 딜러가 블랙잭이라면
+	         lose(); // 플래이어 패배ㅜㅅㅜ
+	         return;
+	      }
 
-		if (voP.getBust()) { // 플레이어가 BUST
-			if (voD.getBust())
-				push(); // 딜러도 BUST 라면 무승부
-			else
-				lose(); // 딜러는 BUST 아니라면 패배
-		} else if (((pScore > dScore) || voD.getBust()))
-			win(); // 플레이어는 BUST가 아니면서 딜러보다 점수가 높거나, 딜러가 BUST면 승리
+	      if (voP.getBust()) { // 플레이어가 BUST
+	         if (voD.getBust())
+	            push(); // 딜러도 BUST 라면 무승부
+	         else
+	            lose(); // 딜러는 BUST 아니라면 패배
+	      } else if (((pScore > dScore) || voD.getBust()))
+	         win(); // 플레이어는 BUST가 아니면서 딜러보다 점수가 높거나, 딜러가 BUST면 승리
 
-		else if (pScore < dScore)
-			lose(); // 플레이어, 딜러는 BUST가 아니면서 플레이어가 딜러보다 점수가 낮으면 패배
+	      else if (pScore < dScore)
+	         lose(); // 플레이어, 딜러는 BUST가 아니면서 플레이어가 딜러보다 점수가 낮으면 패배
 
-		else if (pScore == dScore)
-			push(); // 플레이어, 딜러는 BUST가 아니면서 플레이어와 딜러 점수가 같으면 무승부
+	      else if (pScore == dScore)
+	         push(); // 플레이어, 딜러는 BUST가 아니면서 플레이어와 딜러 점수가 같으면 무승부
 
-		else if (voD.getBj())
-			lose();
+	      else if (voD.getBj())
+	         lose();
 
-	}
-
+	   }
+	
+	
 	// TODO 최선영
 	// 플레이어가 블랙잭으로 이겼을 때 베팅금 계산
 	public void win_bj() {
